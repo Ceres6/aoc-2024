93304fe4d6d6e3df9ff4fb68f74cda1cd11c4fab
Carlos Espa <carlos.espa@nearform.com>
2024-12-04 21:26:32 +0100

    chore(dec2): reduce number of operations in worst-case

--- src/dec2/mod.rs
+++ src/dec2/mod.rs
@@ -7,64 +7,18 @@ enum Direction {
     Decreasing,
 }
 
-pub(crate) fn safe_count() -> i32 {
+pub(crate) fn safe_count() -> usize {
     let bytes = read(PATH).unwrap();
     let reports = String::from_utf8(bytes).unwrap();
-    reports.lines().fold(0, |acc, report| {
-        let mut prev_opt = None;
-        let mut dir_opt = None;
-        for word in report.split_ascii_whitespace() {
-            let level = word.parse::<i32>().unwrap();
-            if let Some(prev) = prev_opt {
-                match dir_opt {
-                    Some(Direction::Decreasing) => match prev - level {
-                        1..=3 => (),
-                        _ => return acc,
-                    },
-                    Some(Direction::Increasing) => match level - prev {
-                        1..=3 => (),
-                        _ => return acc,
-                    },
-                    None => match level - prev {
-                        -3..=-1 => {
-                            dir_opt = Some(Direction::Decreasing);
-                        }
-                        1..=3 => {
-                            dir_opt = Some(Direction::Increasing);
-                        }
-                        _ => return acc,
-                    },
-                }
-            }
-            prev_opt = Some(level);
-        }
-        acc + 1
-    })
+    reports.lines().filter(|&report| is_safe(report.split_ascii_whitespace(), true)).count()
 }
 
-pub(crate) fn dampened_count() -> i32 {
+pub(crate) fn dampened_count() -> usize {
     let bytes = read(PATH).unwrap();
     let reports = String::from_utf8(bytes).unwrap();
-    reports.lines().fold(0, |acc, report| {
-        if is_safe(report.split_ascii_whitespace(), false)
-        {
-            acc + 1
-        } else {
-            for i in 0..report.split_ascii_whitespace().count() {
-                if is_safe(
-                    report
-                        .split_ascii_whitespace()
-                        .enumerate()
-                        .filter(|(j, _)| *j != i)
-                        .map(|(_, w)| w),
-                    true,
-                ) {
-                    return acc + 1;
-                }
-            }
-            acc
-        }
-    })
+    reports.lines().filter(|&report| is_safe(report.split_ascii_whitespace(), false)
+            || is_safe(report.split_ascii_whitespace().rev(), false)
+    ).count()
 }
 
 fn is_safe<'a, I: Iterator<Item = &'a str>>(report: I, mut already_dampened: bool) -> bool {
